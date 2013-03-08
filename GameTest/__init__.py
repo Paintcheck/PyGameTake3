2da361a2fd882777718d3cbef8ce93dfeecb68d4
Dennis Cahill <dennisjcj@gmail.com>
2013-03-08 12:36:54 -0500

    Button Click changes background but it doesnt stay

--- GameTest/__init__.py
+++ GameTest/__init__.py
@@ -1,25 +1,48 @@
 import pygame
+from PIL import Image
 pygame.init()
-#Dennis Branch
-screen = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption("Hello, world!")
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((127, 255, 0))
-clock = pygame.time.Clock()
+# Dennis Branch
 
-font = pygame.font.Font(None, 36)
-text = font.render("Hi Comp 585", 1, (0, 0, 0))
-textpos = text.get_rect()
-textpos.centerx = background.get_rect().centerx
-textpos.centery = background.get_rect().centery
-background.blit(text, textpos)
+#######################################################
+pg = pygame
+clock = pg.time.Clock()
+#######################################################
+
+
+background1 = pg.image.load("TreasureMap.jpg")
+screen1 = pg.display.set_mode((1024, 768))
+#######################################################
+
+background2 = pg.image.load("Aquarium.jpg")
+
+screen2 = pg.display.set_mode((1024, 768))
+
+#######################################################
+
+screenswitch = 0
 keepGoing = True
 while keepGoing:
     clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             keepGoing = False
-                
-    screen.blit(background, (0,0))
-    pygame.display.flip()
+            
+    if screenswitch == 0:
+        screen1.blit(background1, (0,0))
+        pg.display.flip()  
+        #screenswitch = 1
+    else:
+        screen2.blit(background2, (0,0))
+        pg.display.flip()
+        #screenswitch = 0
+        
+    
+    if event.type == pygame.MOUSEBUTTONUP:
+            #screen1.blit(background1, (0,0))
+            #pg.display.flip()  
+        screenswitch = 1       
+    else:
+            #screen2.blit(background2, (0,0))
+            #pg.display.flip()
+        screenswitch = 0
+    
