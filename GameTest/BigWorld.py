9c39108f8f6aa2eab9ea01239c03c5d0405b917b
Dennis Cahill <dennisjcj@gmail.com>
2013-03-20 05:53:47 -0400

    Added a lot. Don't remember all of it.
    Scrolling background that scrolls whenever you reach the edge of the
    screen.
    Collision detection for the fish and the treasure.

--- GameTest/BigWorld.py
+++ GameTest/BigWorld.py
@@ -1,26 +1,31 @@
 import pygame
 #this is a test of the please sync for god's sake
-Ldiverdude = pygame.image.load("Lscubadiversmall.png")
-Rdiverdude = pygame.image.load("Rscubadiversmall.png")
+Ldiverdude = pygame.image.load("Lscubadiversmall.png") # 224 x 188
+Rdiverdude = pygame.image.load("Rscubadiversmall.png") # 224 x 188
 
-Ldiverbuddy = pygame.image.load("Lbuddy.png")
-Rdiverbuddy = pygame.image.load("Rbuddy.png")
+Ldiverbuddy = pygame.image.load("Lbuddy.png") # 224 x 188
+Rdiverbuddy = pygame.image.load("Rbuddy.png") # 224 x 188
 
-Reel = pygame.image.load("Reel.png")
+Reel = pygame.image.load("Reel.png") # 200 x 46
+eel = pygame.image.load("eel.png") # 538 x 125
 
-Lcfishmedium = pygame.image.load("Lcfishmedium.png")
+Lcfishmedium = pygame.image.load("Lcfishmedium.png") # 100 x 45
+Lcfishsmall = pygame.image.load("Lcfishsmall.png") # 50 x 22
+cfish = pygame.image.load("cfish.png") # 476 x 212
 
-Lcfishsmall = pygame.image.load("Lcfishsmall.png")
 
-treasuresmall = pygame.image.load("treasuresmall.png")
+treasuresmall = pygame.image.load("treasuresmall.png") # 120 x 100
 
-treasurebig = pygame.image.load("treasurebig.png")
+treasurebig = pygame.image.load("treasurebig.png") # 472 x 395
 
-AcquariumBackground = pygame.image.load("Aquarium.jpg")
+seafloor = pygame.image.load("seafloor2.jpg") # 6000 x 768
 
 ocean = [135, 206, 250] 
 black = [0, 0, 0]
 # Function to draw our stick figure
+def drawseafloor(screen,x,y):
+    screen.blit(seafloor, (x, y))
+
 def LdrawDiver(screen,x,y):
     screen.blit(Ldiverdude, (x, y))
 def RdrawDiver(screen,x,y):
@@ -34,17 +39,37 @@ def RdrawBuddy(screen,x,y):
 def Rdraweel(screen,x,y):
     screen.blit(Reel, (x, y))
 
+def draweel(screen,x,y):
+    screen.blit(eel, (x, y))
+    
 def Ldrawcfishmedium(screen,x,y):
     screen.blit(Lcfishmedium, (x, y))
     
 def Ldrawcfishsmall(screen,x,y):
     screen.blit(Lcfishsmall, (x, y))
 
+def drawcfish(screen,x,y):
+    screen.blit(cfish, (x, y))
+    
 def drawtreasuresmall(screen,x,y):
     screen.blit(treasuresmall, (x, y))
     
 def drawtreasurebig(screen,x,y):
-    screen.blit(treasurebig, (x, y))  
+    screen.blit(treasurebig, (x, y))
+
+def collision(object1, object2):
+    x1 = object1[0]
+    y1 = object1[1]
+    dimx1 = object1[2]
+    dimy1 = object1[3]
+    x2 = object2[0]
+    y2 = object2[1]
+    dimx2 = object2[2]
+    dimy2 = object2[3]
+    if (x1 > x2 and x1 < x2 + dimx2 and y1 > y2 and y1 < y2 + dimy2) or (x1 + dimx1 > x2 and x1 + dimx1 < x2 + dimx2 and y1 > y2 and y1 < y2 + dimy2) or (x1 + dimx1 > x2 and x1 + dimx1 < x2 + dimx2 and y1 + dimy1 > y2 and y1 + dimy1 < y2 + dimy2) or (x1 > x2 and x1 < x2 + dimx2 and y1 + dimy1 > y2 and y1 + dimy1 < y2 + dimy2) or (x2 > x1 and x2 < x1 + dimx1 and y2 > y1 and y2 < y1 + dimx1) or (x2 + dimx2 > x1 and x2 + dimx2 < x1 + dimx1 and y2 > y1 and y2 < y1 + dimy1) or (x2 + dimx2 > x1 and x2 + dimx2 < x1 + dimx1 and y2 + dimy2 > y1 and y2 + dimy2 < y1 + dimy1) or (x2 > x1 and x2 < x1 + dimx1 and y2 + dimy2 > y1 and y2 + dimy2 < y1 + dimy1):
+        return True
+    else:
+        return False
     
 # Setup
 pygame.init()
@@ -84,10 +109,19 @@ y_coordcfishmedium = 200
 x_coordcfishsmall = 1024 + 250 + 100
 y_coordcfishsmall = 200 + 11
 
+x_coordtreasure = 3000
+y_coordtreasure = 768 - 100
+treasure_c = [x_coordtreasure, y_coordtreasure, 120, 100]
+
+x_coordback = 0
+y_coordback = 0
+
 #Direction to face
 direction = 1
 buddydirection =1
-speed=10
+speed = 30
+eelspeed = 20
+cfishspeed = 20
 
 # -------- Main Program Loop -----------
 while done==False:
@@ -132,10 +166,26 @@ while done==False:
  
     if x_coord < 0:
         x_coord = 0
+        x_coordback = x_coordback + speed
+        if x_coordback > 0:
+            x_coordback = 0
+        else:
+            x_coordeel = x_coordeel + speed
+            x_coordcfishmedium = x_coordcfishmedium + speed
+            x_coordcfishsmall = x_coordcfishsmall + speed
+            x_coordtreasure = x_coordtreasure + speed
     if y_coord < 0:
         y_coord = 0
     if x_coord > 1024-224:
         x_coord = 1024-224
+        x_coordback = x_coordback - speed
+        if x_coordback < -6000 + 1024:
+            x_coordback = -6000 + 1024
+        else:
+            x_coordeel = x_coordeel - speed
+            x_coordcfishmedium = x_coordcfishmedium - speed
+            x_coordcfishsmall = x_coordcfishsmall - speed
+            x_coordtreasure = x_coordtreasure - speed
     if y_coord > 768-188:
         y_coord = 768-188
         
@@ -167,10 +217,11 @@ while done==False:
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
     screen.fill(ocean)
-    screen.blit(AcquariumBackground, (0,0))
+    #screen.blit(SeafloorBackground, (0,0))
     
+    drawseafloor(screen, x_coordback, y_coordback)
     
-    drawtreasuresmall(screen, 1024 - 120, 768 - 100)
+    drawtreasuresmall(screen, x_coordtreasure, y_coordtreasure)
     
     if buddydirection == 1:
             RdrawBuddy(screen, x_coordbuddy, y_coordbuddy)
@@ -182,25 +233,36 @@ while done==False:
     else:
         LdrawDiver(screen, x_coord, y_coord)
         
-    x_coordeel = x_coordeel + 5
-    if x_coordeel > 1024:
-        x_coordeel = -500
+    x_coordeel = x_coordeel + eelspeed
+    if x_coordeel > x_coordback + 6000:
+        x_coordeel = x_coordback
     Rdraweel(screen, x_coordeel, y_coordeel)
     
-    x_coordcfishmedium = x_coordcfishmedium - 2
-    if x_coordcfishmedium < -100:
-        x_coordcfishmedium = 1024 + 500
+    x_coordcfishmedium = x_coordcfishmedium - cfishspeed
+    if x_coordcfishmedium < x_coordback - 100 - 50:
+        x_coordcfishmedium = x_coordback + 6000
     Ldrawcfishmedium(screen, x_coordcfishmedium, y_coordcfishmedium)
     
-    x_coordcfishsmall = x_coordcfishsmall - 2
-    if x_coordcfishsmall < -100 + 100:
-        x_coordcfishsmall = 1024 + 500 + 100
+    x_coordcfishsmall = x_coordcfishsmall - cfishspeed
+    if x_coordcfishsmall < x_coordback - 100 - 50 + 100:
+        x_coordcfishsmall = x_coordback + 6000 + 100
     Ldrawcfishsmall(screen, x_coordcfishsmall, y_coordcfishsmall)
  
-    if x_coord == 1024 - 224 and y_coord == 768 - 188:
-        drawtreasurebig(screen, 276, 187)
-        
+    #if x_coord == 1024 - 224 and y_coord == 768 - 188:
+    #    drawtreasurebig(screen, 276, 187)
     
+    player_c = [x_coord, y_coord, 224, 188]
+    buddy_c = [x_coordbuddy, y_coordbuddy, 224, 188]
+    eel_c = [x_coordeel, y_coordeel, 200, 46]
+    treasure_c = [x_coordtreasure, y_coordtreasure, 120, 100]
+    cfishmedium_c = [x_coordcfishmedium, y_coordcfishmedium, 100, 45]
+    
+    if collision(player_c, treasure_c):
+        drawtreasurebig(screen, 276, 187)
+    if collision(player_c, eel_c):
+        draweel(screen, 243, 322)
+    if collision(player_c, cfishmedium_c):
+        drawcfish(screen, 274, 278)
  
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
       
